cb0c59a9e74c6e8367cf9f4c60fb105aae0bf218
akashmurumkar <akashmurumkar@outlook.com>
2025-05-20 15:41:53 +0530

    20 May Code

---
+++ src/main/java/com/autoramming/FixTheCreditCard.java
@@ -0,0 +1,31 @@
+package com.autoramming;
+
+public class FixTheCreditCard {
+
+    private double balance;
+    private final double creditLimit;
+    private final double interest;
+
+    public FixTheCreditCard(double creditLimit, double interest) {
+        this.creditLimit = creditLimit;
+        this.interest = interest;
+        this.balance = 0;
+    }
+
+    public boolean deposit(double amount) {
+        if(amount > creditLimit) {
+            return false;
+        }
+        this.balance += amount;
+        return true;
+    }
+
+    public double calculateInterest() {
+        return (this.interest / 12 / 100) * this.balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+}
